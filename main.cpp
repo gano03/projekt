c8597823c1abb39258335557fc672f938ce5ea4e
gano03 <pganobis@interia.pl>
2020-07-03 16:44:12 +0200

    init 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
-using namespace std;
-int main ()
-{
-  cout << "Dzien dobry\n";
+//Paweł Ganobis
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window{sf::VideoMode(800,600 ), "Arkanoid"};
+    sf::CircleShape shape(150.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) 
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display(); // test1
+    }
 
     return 0;
 }
